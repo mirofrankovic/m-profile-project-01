e47e3676f1811477ca7bda4ff6be65ac3ecac9fe
Miro Frankovic <miro.frankovic@gmail.com>
2021-04-14 23:48:02 +0000

    Fix iPhone mode and resize image

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,15 @@
+$(document).ready(function () {
+    $('.header').height($(window).height());
+
+})
+// var myModal = new bootstrap.Modal(document.getElementById('myModal'), options)
 // var myModal = document.getElementById('myModal')
 // var myInput = document.getElementById('myInput')
 
 // myModal.addEventListener('shown.bs.modal', function () {
-//   myInput.focus()
+//     myInput.focus()
 // })
+
+var myModal = document.getElementById('myModal')
+var modal = bootstrap.Modal.getInstance(myModal)
+// console.log(modal) // null
